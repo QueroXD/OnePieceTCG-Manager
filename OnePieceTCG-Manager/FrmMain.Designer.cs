9f16e985763287ffa14a1d3db3a0ac9c8a0bc5aa
QueroXD <alvaroqueroariza@gmail.com>
2025-11-04 11:41:45 +0100

    Add main form menu and icon resources
    Introduced a menu strip to FrmMain with options for settings, stock management, and economy. Added FrmMain.resx for resource management and included a custom application icon. Updated project file to include new resources.

--- OnePieceTCG-Manager/FrmMain.Designer.cs
+++ OnePieceTCG-Manager/FrmMain.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.ajustesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testConexiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verStockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.añadirStockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.economiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ingresosEstimadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ingresosGanadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ajustesToolStripMenuItem,
+            this.gestiónToolStripMenuItem,
+            this.economiaToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(800, 30);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menu";
+            // 
+            // ajustesToolStripMenuItem
+            // 
+            this.ajustesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testConexiónToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.ajustesToolStripMenuItem.Name = "ajustesToolStripMenuItem";
+            this.ajustesToolStripMenuItem.Size = new System.Drawing.Size(70, 26);
+            this.ajustesToolStripMenuItem.Text = "Ajustes";
+            // 
+            // testConexiónToolStripMenuItem
+            // 
+            this.testConexiónToolStripMenuItem.Name = "testConexiónToolStripMenuItem";
+            this.testConexiónToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.testConexiónToolStripMenuItem.Text = "Test Conexión";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // gestiónToolStripMenuItem
+            // 
+            this.gestiónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.verStockToolStripMenuItem,
+            this.añadirStockToolStripMenuItem});
+            this.gestiónToolStripMenuItem.Name = "gestiónToolStripMenuItem";
+            this.gestiónToolStripMenuItem.Size = new System.Drawing.Size(73, 26);
+            this.gestiónToolStripMenuItem.Text = "Gestión";
+            // 
+            // verStockToolStripMenuItem
+            // 
+            this.verStockToolStripMenuItem.Name = "verStockToolStripMenuItem";
+            this.verStockToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.verStockToolStripMenuItem.Text = "Ver Stock";
+            // 
+            // añadirStockToolStripMenuItem
+            // 
+            this.añadirStockToolStripMenuItem.Name = "añadirStockToolStripMenuItem";
+            this.añadirStockToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.añadirStockToolStripMenuItem.Text = "Añadir Stock";
+            // 
+            // economiaToolStripMenuItem
+            // 
+            this.economiaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ingresosEstimadosToolStripMenuItem,
+            this.ingresosGanadosToolStripMenuItem});
+            this.economiaToolStripMenuItem.Name = "economiaToolStripMenuItem";
+            this.economiaToolStripMenuItem.Size = new System.Drawing.Size(89, 26);
+            this.economiaToolStripMenuItem.Text = "Economia";
+            // 
+            // ingresosEstimadosToolStripMenuItem
+            // 
+            this.ingresosEstimadosToolStripMenuItem.Name = "ingresosEstimadosToolStripMenuItem";
+            this.ingresosEstimadosToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ingresosEstimadosToolStripMenuItem.Text = "Ingresos estimados";
+            // 
+            // ingresosGanadosToolStripMenuItem
+            // 
+            this.ingresosGanadosToolStripMenuItem.Name = "ingresosGanadosToolStripMenuItem";
+            this.ingresosGanadosToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ingresosGanadosToolStripMenuItem.Text = "Ingresos Ganados";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.menu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menu;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "One Piece TCG - Manager";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem ajustesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testConexiónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestiónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verStockToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem añadirStockToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem economiaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ingresosEstimadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ingresosGanadosToolStripMenuItem;
     }
 }
 
